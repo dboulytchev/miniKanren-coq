974a6d144ab602981e0d71ff6a318eabd0a84b64
rozplokhas <rozplokhas@gmail.com>
2018-05-30 23:43:34 +0300

    Proofs to all lemmas about unification step except the core one

--- Unify.v
+++ Unify.v
@@ -18,16 +18,20 @@ Definition union (l1 l2 : list name) : list name := nodup eq_nat_dec (l1 ++ l2).
 
 (* Some lemmas for lists as sets *)
 Lemma union_NoDup: forall (l1 l2 : list name), NoDup (union l1 l2).
-Proof. admit. Admitted.
+Proof. intros. unfold union. apply NoDup_nodup. Qed.
 
 Lemma union_In: forall (l1 l2 : list name) (n : name), In n (union l1 l2) <-> In n l1 \/ In n l2.
-Proof. admit. Admitted.
-                       
+Proof.
+  intros. unfold union. split.
+  * intros. apply in_app_or. apply nodup_In in H. assumption.
+  * intros. apply nodup_In. apply in_or_app. assumption.
+Qed.
+
 Lemma lt_length:
   forall (l1 l2 : list name),
     NoDup l1 -> NoDup l2 -> (forall n, In n l1 -> In n l2) -> (exists n, In n l2 /\ ~ (In n l1)) -> length l1 < length l2.
 Proof. admit. Admitted.
-    
+
 (* Free variables *)
 Fixpoint fv (t : term) : list name :=
   match t with
@@ -38,18 +42,49 @@ Fixpoint fv (t : term) : list name :=
 
 (* fv returns NoDup *)
 Lemma fv_NoDup: forall t,  NoDup (fv t).
-Proof. admit. Admitted.
+Proof.
+  intros. destruct t; unfold fv.
+  * constructor.
+    + auto.
+    + constructor.
+  * constructor.
+  * apply union_NoDup.
+Qed.
 
 (* Occurs property *)
-Fixpoint occurs (n : name) (t : term) : bool :=
+Definition occurs (n : name) (t : term) : bool :=
   if find (fun x => if eq_nat_dec x n then true else false) (fv t)
   then true
   else false.
 
 (* In follows from occurs *)
 Lemma occurs_In: forall t n, occurs n t = true <-> In n (fv t).
-Proof. admit. Admitted.
-  
+Proof.
+  intros. unfold occurs.
+  destruct (find (fun x : nat => if Nat.eq_dec x n then true else false)
+                 (fv t)) eqn:eq.
+  * split.
+    + intros. generalize dependent eq. induction (fv t).
+      - intros. inversion eq.
+      - intros. unfold find in eq. destruct (Nat.eq_dec a n).
+        { inversion eq; subst. unfold In. left. reflexivity. }
+        { apply IHl in eq. unfold In. right. assumption.  }
+    + auto.
+  * split.
+    + intros. inversion H.
+    + intros.
+      induction (fv t).
+      - inversion H.
+      - unfold find in eq. destruct (Nat.eq_dec a n).
+        { inversion eq. }
+        { apply IHl.
+          * assumption.
+          * destruct H.
+            + contradiction.
+            + assumption. }
+Qed.
+
+
 (******************** Substitutions **********************)
 (* Substitution *)
 Definition subst : Type := list (name * term).
@@ -74,7 +109,21 @@ Fixpoint apply (s : subst) (t : term) : term :=
 
 (* Free variables in the result of substitution application *)
 Lemma apply_fv: forall t s n m,  In m (fv (apply [(n, s)] t)) -> In m (fv s) \/ In m (fv t).
-Proof. admit. Admitted.
+Proof.
+  induction t.
+  * intros. unfold apply in H. unfold image in H. destruct (Nat.eq_dec n0 n).
+    + left. assumption.
+    + right. assumption.
+  * intros. inversion H.
+  * intros. unfold apply in H. fold apply in H.
+    unfold fv in H. fold fv in H. apply union_In in H. destruct H.
+    + apply IHt1 in H. destruct H.
+      - left. assumption.
+      - right. unfold fv. fold fv. apply union_In. left. assumption.
+    + apply IHt2 in H. destruct H.
+      - left. assumption.
+      - right. unfold fv. fold fv. apply union_In. right. assumption.
+Qed.
 
 (* Generality *)
 Definition more_general (m s : subst) : Prop :=
@@ -155,19 +204,124 @@ Definition unification_step_ok t1 t2 n s := unification_step t1 t2 = Subst n s.
 
 Lemma unification_step_fv: forall t1 t2 s n ,
     unification_step_ok t1 t2 n s -> (forall m, In m (fv s) -> In m (fv t1) \/ In m (fv t2)).
-Proof. admit. Admitted. 
+Proof.
+  assert (invCr: forall n0 n1 t0 t1, create n0 t0 = Subst n1 t1 -> t0 = t1).
+  { intros. unfold create in H. destruct (occurs n0 t0).
+    - inversion H.
+    - inversion H. reflexivity. }
+  induction t1.
+  * intros. unfold unification_step_ok in H. destruct t2; unfold unification_step in H.
+    + destruct (Nat.eq_dec n n1).
+      - inversion H.
+      - apply invCr in H; subst. right. assumption.
+    + apply invCr in H; subst. right. assumption.
+    + apply invCr in H; subst. right. assumption.
+  * intros. unfold unification_step_ok in H. destruct t2; unfold unification_step in H.
+    + apply invCr in H; subst. left. assumption.
+    + destruct (Nat.eq_dec n n1); inversion H.
+    + inversion H.
+  * intros. unfold unification_step_ok in H. destruct t2; unfold unification_step in H.
+    + apply invCr in H; subst. left. assumption.
+    + inversion H.
+    + fold unification_step in H. destruct (Nat.eq_dec n n1).
+      - destruct (unification_step t1_1 t2_1) eqn:eq.
+        { inversion H. }
+        { unfold unification_step_ok in IHt1_2. apply IHt1_2 with (m := m) in H.
+          - destruct H.
+            + left. unfold fv. fold fv. apply union_In. right. assumption.
+            + right. unfold fv. fold fv. apply union_In. right. assumption.
+          - assumption. }
+        { inversion H; subst. unfold unification_step_ok in IHt1_1.
+          apply IHt1_1 with (m := m) in eq.
+          - destruct eq.
+            + left. unfold fv. fold fv. apply union_In. left. assumption.
+            + right. unfold fv. fold fv. apply union_In. left. assumption.
+          - assumption. }
+      - inversion H.
+Qed.
 
 Lemma unification_step_subst_wf:
   forall t1 t2 s n, unification_step_ok t1 t2 n s -> ~ In n (fv s).
-Proof. admit. Admitted.
+Proof.
+  intros. assert (exists m t, create m t = Subst n s).
+  {
+    generalize dependent t2. induction t1; intros.
+    * destruct t2; unfold unification_step_ok in H; unfold unification_step in H.
+      + destruct (Nat.eq_dec n0 n1).
+         - inversion H.
+         - eexists. eexists. eapply H.
+      + eexists. eexists. eapply H.
+      + eexists. eexists. eapply H.
+    * destruct t2; unfold unification_step_ok in H; unfold unification_step in H.
+      + eexists. eexists. eapply H.
+      + destruct (Nat.eq_dec n0 n1); inversion H.
+      + inversion H.
+    * destruct t2; unfold unification_step_ok in H; unfold unification_step in H.
+      + eexists. eexists. eapply H.
+      + inversion H.
+      + destruct (Nat.eq_dec n0 n1).
+        - fold unification_step in H. destruct (unification_step t1_1 t2_1) eqn:eq.
+          { inversion H. }
+          { eapply IHt1_2. unfold unification_step_ok. eapply H. }
+          { inversion H; subst. eapply IHt1_1. unfold unification_step_ok.
+            eapply eq. }
+        - inversion H.
+  }
+  destruct H0. destruct H0. unfold create in H0. destruct (occurs x x0) eqn:eq.
+  - inversion H0.
+  - inversion H0; subst. intros CH. apply occurs_In in CH. rewrite eq in CH.
+    inversion CH.
+Qed.
 
 Lemma unification_step_subst_occurs:
   forall t1 t2 s n, unification_step_ok t1 t2 n s -> In n (fv t1) \/ In n (fv t2).
-Proof. admit. Admitted.
+Proof.
+  assert (invCr: forall n0 n1 t0 t1, create n0 t0 = Subst n1 t1 -> n0 = n1).
+  { intros. unfold create in H. destruct (occurs n0 t0).
+    - inversion H.
+    - inversion H. reflexivity. }
+  assert (fvInFv: forall n, In n (fv (Var n))).
+  { unfold fv. unfold In. left. reflexivity. }
+  induction t1.
+  * intros. unfold unification_step_ok in H. destruct t2; unfold unification_step in H.
+    + destruct (Nat.eq_dec n n1).
+      - inversion H.
+      - apply invCr in H; subst. left. apply fvInFv.
+    + apply invCr in H; subst. left. apply fvInFv.
+    + apply invCr in H; subst. left. apply fvInFv.
+  * intros. unfold unification_step_ok in H. destruct t2; unfold unification_step in H.
+    + apply invCr in H; subst. right. apply fvInFv.
+    + destruct (Nat.eq_dec n n1); inversion H.
+    + inversion H.
+  * intros. unfold unification_step_ok in H. destruct t2; unfold unification_step in H.
+    + apply invCr in H; subst. right. apply fvInFv.
+    + inversion H.
+    + fold unification_step in H. destruct (unification_step t1_1 t2_1) eqn:eq.
+      - destruct (Nat.eq_dec n n1); inversion H.
+      - destruct (Nat.eq_dec n n1).
+        { unfold unification_step_ok in IHt1_2. apply IHt1_2 in H. destruct H.
+          * left. unfold fv. fold fv. apply union_In. right. assumption.
+          * right. unfold fv. fold fv. apply union_In. right. assumption. }
+        inversion H.
+      - destruct (Nat.eq_dec n n1); inversion H; subst.
+        unfold unification_step_ok in IHt1_1. apply IHt1_1 in eq. destruct eq.
+        { left. unfold fv. fold fv. apply union_In. left. assumption. }
+        right. unfold fv. fold fv. apply union_In. left. assumption.
+Qed.
 
 Lemma unification_step_subst_elims: forall s t n, In n (fv (apply [(n, s)] t)) -> In n (fv s).
-Proof. admit. Admitted.
-  
+Proof.
+  intros s t n. induction t.
+  * unfold apply. unfold image. destruct (Nat.eq_dec n n0).
+    + auto.
+    + unfold fv. intros. exfalso. inversion H.
+      - apply n1. symmetry. assumption.
+      - inversion H0.
+  * intros. inversion H.
+  * intros. unfold apply in H. fold apply in H. unfold fv in H. fold fv in H.
+    apply union_In in H. destruct H; auto.
+Qed.
+
 Lemma unification_step_decreases_fv:
   forall t1 t2 s n,
     unification_step_ok t1 t2 n s ->
@@ -192,4 +346,3 @@ Proof.
       apply unification_step_subst_elims in H0;
       apply unification_step_subst_wf in USok; auto.
 Qed.
-      
