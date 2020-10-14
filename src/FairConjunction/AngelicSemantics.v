7be9d555a4a21ffb7290d937a8b7ca784b3fd7f6
Dmitry Boulytchev <dboulytchev@gmail.com>
2020-10-14 23:09:26 +0300

    Added angelic semantics

---
+++ src/FairConjunction/AngelicSemantics.v
@@ -0,0 +1,12 @@
+Require Import List.
+Import ListNotations.
+Require Import Coq.Lists.ListSet.
+Require Import Coq.Program.Equality.
+Require Import Omega.
+Require Import Extraction.
+
+Require Import Unification.
+Require Import Streams.
+Require Import Language.
+Require Import DenotationalSem.
+Require Import OperationalSem.
